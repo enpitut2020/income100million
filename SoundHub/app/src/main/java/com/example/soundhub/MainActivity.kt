51308787b6e60aebf2dae868a64c1f2f01533bc5
naoiyuuma <s1811369@coins.tsukuba.ac.jp>
2020-07-24 11:28:59 +0900

    遷移

--- SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
+++ SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
@@ -5,44 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.soundhub.R.id.search_button
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_search.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(search_button)
+        val button1 = findViewById<Button>(R.id.search_button)
+        val button2 = findViewById<Button>(R.id.button2)
+        button1.setOnClickListener {
 
-        button.setOnClickListener {
-            // ここに任意の処理を実装する
-            // 妥当かどうか
-            /*
-            var isValid = true
-
-            val priceEditText = findViewById<EditText>(R.id.price)
-            val priceText = priceEditText.text.toString()
-
-            if (priceText.isEmpty()) {
-                // 定価が未入力
-                priceEditText.error = getString(R.string.price_error)
-                isValid = false
-            }
-
-            val discountEditText = findViewById<EditText>(R.id.discount)
-            val discountText = discountEditText.text.toString()
-
-            if (discountText.isEmpty()) {
-                // 割引率が未入力
-                discountEditText.error = getString(R.string.discount_error)
-                isValid = false
-            }
-            */
 
                 // ここで画面遷移を行う
                 val intent = Intent(this, SearchActivity::class.java)
-                //intent.putExtra("price", price)
-                //intent.putExtra("discount", discount) //値渡し
+
                 startActivity(intent)
         }
+
+        button2.setOnClickListener {
+
+            // ここで画面遷移を行う
+            val intent = Intent(this, EditActivity::class.java)
+
+            startActivity(intent)
+        }
+
     }
 }
