1100e2004439d385d4159132aa50b6de4fcd2ea2
knjyITF <ymmt8201@gmail.com>
2020-07-27 11:09:33 +0900

    おけ

---
+++ SoundHub/app/src/main/java/com/example/soundhub/EditPlayList.kt
@@ -0,0 +1,11 @@
+package com.example.soundhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPlayList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_play_list)
+    }
+}
