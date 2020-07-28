3c29ab41a89d911b49841880b41e4734c50554df
ymmt <ymmt8201@gmail.com>
2020-07-28 19:33:09 +0900

    デプロイしたもの置いときます

--- SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
+++ SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
@@ -11,58 +11,6 @@ import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 
-/*
-class MainActivity : AppCompatActivity() {
-    private val TAG = "DocSnippets"
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val db = FirebaseFirestore.getInstance()
-        //Create a new user with a first and last name
-        val user = hashMapOf(
-            "first" to "Ada",
-            "last" to "Lovelace",
-            "born" to 1815
-        )
-
-
-        db.collection("cities").document("new-city-id").set(user)
-// Add a new document with a generated ID
-            db.collection("users")
-            .add(user)
-            .addOnSuccessListener { documentReference ->
-                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
-            }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error adding document", e)
-            }
-
-
-        val button1 = findViewById<Button>(R.id.search_button)
-        val button2 = findViewById<Button>(R.id.edit_button)
-        button1.setOnClickListener {
-
-
-            // ここで画面遷移を行う
-            val intent = Intent(this, SearchActivity::class.java)
-
-            startActivity(intent)
-        }
-
-        button2.setOnClickListener {
-
-            // ここで画面遷移を行う
-            val intent2 = Intent(this, EditPlayList::class.java)
-
-            startActivity(intent2)
-        }
-
-    }
-*/
-
-
 class MainActivity : AppCompatActivity() {
 
     private var mToolbar: Toolbar? = null
