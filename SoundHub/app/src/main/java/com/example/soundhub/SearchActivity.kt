93da05e864f8525e262f4477dbc9055c26af7099
knjyITF <ymmt8201@gmail.com>
2020-07-28 22:01:16 +0900

    とりあえずpush

--- SoundHub/app/src/main/java/com/example/soundhub/SearchActivity.kt
+++ SoundHub/app/src/main/java/com/example/soundhub/SearchActivity.kt
@@ -3,6 +3,7 @@ package com.example.soundhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.*
@@ -10,7 +11,9 @@ import com.google.firebase.firestore.FirebaseFirestore
 
 class SearchActivity : AppCompatActivity() {
     private var searchT = ""
-    private val db = FirebaseFirestore.getInstance().document("playLists/savePlayList")
+    private val db = FirebaseFirestore.getInstance()
+
+    private val TAG = "DocSnippets"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,10 +42,47 @@ class SearchActivity : AppCompatActivity() {
     }
 
     private fun show() {
-        val texts = searchTexts(this, searchT)
         val listView = findViewById<ListView>(R.id.listView_s)
-        listView.adapter = ArrayAdapter<String>(this,
-            R.layout.list_text_row, R.id.textView, texts
-        )
+
+        if(searchT.equals("")) {
+            db.collection("playLists3")
+                .get()
+                .addOnSuccessListener { result ->
+                    val listtt = mutableListOf<String>()
+                    for (document in result) {
+                        val title = document.toObject(DataItems::class.java).title
+                        listtt.add(title)
+                    }
+                    listView.adapter = ArrayAdapter<String>(
+                        this,
+                        R.layout.list_text_row, R.id.textView, listtt
+                    )
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "Error getting documents: ", exception)
+                }
+        }else{
+            db.collection("playLists3")
+                .whereArrayContains("title", searchT)
+                .get()
+                .addOnSuccessListener { result ->
+                    val listtt = mutableListOf<String>()
+                    for (document in result) {
+                        val title = document.toObject(DataItems::class.java).title
+                        listtt.add(title)
+                    }
+                    listView.adapter = ArrayAdapter<String>(
+                        this,
+                        R.layout.list_text_row, R.id.textView, listtt
+                    )
+                }
+                .addOnFailureListener { exception ->
+                    Log.d(TAG, "Error getting documents: ", exception)
+                }
+        }
+
+
+        val texts = searchTexts(this, searchT)
+
     }
 }
