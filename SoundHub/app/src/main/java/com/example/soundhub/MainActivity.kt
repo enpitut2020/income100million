943db5ccd62c8566ca0543acbb4af89c98515143
Suree33 <satodai0315@gmail.com>
2020-07-28 10:37:56 +0900

    Add Top App Bar

--- SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
+++ SoundHub/app/src/main/java/com/example/soundhub/MainActivity.kt
@@ -3,26 +3,23 @@ package com.example.soundhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
-import com.example.soundhub.R.id.search_button
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_search.*
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private var mToolbar: Toolbar? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button1 = findViewById<Button>(R.id.search_button)
+        mToolbar = findViewById(R.id.main_toolbar)
+        setSupportActionBar(mToolbar)
+
         val button2 = findViewById<Button>(R.id.edit_button)
-        button1.setOnClickListener {
-
-
-            // ここで画面遷移を行う
-            val intent = Intent(this, SearchActivity::class.java)
-
-            startActivity(intent)
-        }
 
         button2.setOnClickListener {
 
@@ -31,6 +28,23 @@ class MainActivity : AppCompatActivity() {
 
             startActivity(intent2)
         }
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    // Top App Barのボタンから画面遷移する処理
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.main_menu_search_button) {
+
+            // ここで画面遷移を行う
+            val intent = Intent(this, SearchActivity::class.java)
+
+            startActivity(intent)
+        }
+        return true
     }
 }
